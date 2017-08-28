b387d90b2f759568891ad140f6024b489ec28c88
Michael Miller <michael.miller@bilendo.de>
2017-08-28 23:59:41 +0200

    punctuation

--- src/reducers/reducer_topics.js
+++ src/reducers/reducer_topics.js
@@ -2,53 +2,53 @@ export default function() {
   return [
     {
         title: "About",
-        subTitle1Context: ["A junior software developer, often found cracking edge cases, nesting callbacks and scouring docs for matchers.", "Interested in how technology can be used to solve inefficiencies through a combination of creative and technical means, I've spent the past year honing my coding skills, first at Makers Academy - Europe's leading developer bootcamp - and now at Bilendo - where we're challenging how Europe handles accounts management."]
+        subTitle1Context: ["A junior software developer, often found cracking edge cases, nesting callbacks and scouring docs for matchers.", "Interested in how technology can be used to solve inefficiencies through a combination of creative and technical means, I've spent the past year honing my coding skills, first at Makers Academy—Europe's leading developer bootcamp—and now at Bilendo, where we're challenging how Europe handles accounts management."]
     },
     {
         title: "Skills",
         subTitle1: "Initiative:",
-        subTitle1Context: ["I am proactive when it comes to finding ways to improve productivity and increase value.", "It is this quality that has driven many of my choices, both in my academic and professional life - receiving scholarships to China and the US while an undergraduate and later, successfully pitching ideas to major brands like Warner Music Group, Illva Saronno and more. My knack for identifying opportunities continues to benefit me as I seek ways to bring value to my teammates."],
+        subTitle1Context: ["I am proactive when it comes to finding ways to improve productivity and increase value.", "It's this quality that has driven many of my choices, both in my academic and professional life—seeking scholarships in China and the US as an undergraduate and later, successfully pitching ideas to major brands like Warner Music Group, Illva Saronno and more.", "My knack for identifying opportunities continues to benefit me as I seek ways to bring value to my teammates."],
         subTitle2:"Team Work:",
         subTitle2Context: ["I have a track record for going to lengths to ensure that the finished product best represents the extent of what my team can achieve, whether that requires working late nights and weekends, or foregoing my own ideas to get behind another's."]
     },
     {
         title: "Projects",
         eduLink1: '<a href=\"https://github.com/road-to-code/road-to-code\">Road to Code<\/a>',
-        subTitle1Context: ["Given a brief to build a game that would introduce children to the foundations of programming, our team of four junior developers made Road to Code - an interactive grid-based game that requires the player to get across a map by programming directions for their character to follow. Interested in the challenge, we decided against using any frameworks. Instead, we wrote each line of code and favoured vanilla JavaScript for our frontend."],
+        subTitle1Context: ["Given a brief to build a game that would introduce children to the foundations of programming, our team of four junior developers made Road to Code—an interactive grid-based game that requires the player to get across a map by programming directions for their character to follow. Interested in the challenge, we decided against using any frameworks. Instead, we wrote each line of code and favoured vanilla JavaScript for our frontend."],
         eduLink2: '<a href=\"https://github.com/mjosephmiller/codey-gonzalez\">Codey Gonzalez<\/a>',
         subTitle2Context: ["Participating in a three-day developer-tool hackathon, our team of five junior developers, aimed to improve coders' speed, accuracy and familiarity with best-practice formatting. Defining our MVP early, we implemented agile development to deliver the product on time and to specification."]
     },
     {
         title: "Education",
         eduLink1: '<a href=\"http://www.makersacademy.com\">Makers Academy<\/a>',
-        dates: "July - October 2016",
+        dates: "July–October 2016",
         subTitle1Context: ["A 16-week software development bootcamp"],
         bullets: ["Object-oriented programming", "Agile Methodologies", "Test-Driven Development", "Software Craftsmanship", "Ruby, Rails, Javascript, Node.js", "RSpec, Minitest, Capybara, Jasmine, Mocha, Zombie"],
         eduLink2: '<a href=\"https://www.kcl.ac.uk\">King\'s College London<\/a>',
-        universityDates:"2010 - 2013",
+        universityDates:"2010–2013",
         course:"English Language & Literature, BA Hons (2:1)",
         subTitle2Context: ["Awarded a scholarship to study at the University of Pennsylvania, where I spent my second year of university."]
     },
     {
         title: "Experience",
-        subTitle1: "Programmer - Bilendo",
-        dates: "2016 - Present",
+        subTitle1: "Programmer – Bilendo",
+        dates: "2016–Present",
         subTitle1Summary: "Bilendo is a cloud-based solution for accounts receivables management.",
         bullets: ["Full-stack development in Ruby, Rails, jQuery, Redis, Postgres, AWS, HTML, CSS", "Build and maintain features that improve scalability and performance", "Follow clean code practices with test-driven, agile development", "Work with stakeholders to identify pain points and build user-centered solutions", "Evaluate and implement third-party APIs, including Fastbill, Zuora, Teamleader and sevDesk."],
-        subTitle2: "Digital Content Creative - Freelance",
-        universityDates: "2014 - 2016",
+        subTitle2: "Digital Content Creative – Freelance",
+        universityDates: "2014–2016",
         subTitle2Summary: "Companies: The Weinstein Company, Hanley NYC, EMMAMILLER.COM",
-        bullets2: ["Devised growth strategies for social platforms, including Instagram, Twitter, Facebook, Snapchat and Youtube.", "Worked with editorial and product teams to deliver cross-platform content - including video - to engage the brands' respective audiences, often to tight deadlines.", "Identifying a gap in my skillset, I taught myself to use Adobe Photoshop, InDesign and Premiere Pro.", "Managed collaborations with brands, such as Warner Music Group at Coachella Music Festival, Tom Ford Beauty at New York Fashion Week and Illva Saronno."]
+        bullets2: ["Devised growth strategies for social platforms, including Instagram, Twitter, Facebook, Snapchat and Youtube.", "Worked with editorial and product teams to deliver cross-platform content—including video—to engage the brands' respective audiences, often to tight deadlines.", "Identifying a gap in my skillset, I taught myself to use Adobe Photoshop, InDesign and Premiere Pro.", "Managed collaborations with brands, such as Warner Music Group at Coachella Music Festival, Tom Ford Beauty at New York Fashion Week and Illva Saronno."]
     },
     {
         title: "Talks & Publications",
         subTitle1: "Titles: SID, BRIC, Baku, The Financial Times",
         subTitle1Summary: "Cover business, tech and lifestyle, as well as talent interviews. Recent contributions include:",
-        bullets: ["Rolls Royce: The Business of Bespoke Manufacturing (BRIC, I)", "Jim Sturgess for SID Magazine (Cover - SID, VI)", "Studying in the USA - Q&A (King's College London, 2015)"]
+        bullets: ["Rolls Royce: The Business of Bespoke Manufacturing (BRIC, I)", "Jim Sturgess for SID Magazine (Cover – SID, VI)", "Studying in the USA – Q&A (King's College London, 2015)"]
     },
     {
         title: "Interests",
-        subTitle1Context: ["I'm a regular attendee on the meetup circuit (TwitterDev, Silicon Drinkabout and have even been known to attend a Saturday morning Katathon).", "When I'm not debugging, refactoring or otherwise getting to grips with the latest technologies, I love to get outdoors. I'm a keen swimmer and a slightly less than enthusiastic runner, though I did struggle through the London Marathon this year - I'm nothing if not persevering."]
+        subTitle1Context: ["I'm a regular attendee on the meetup circuit (TwitterDev, Silicon Drinkabout and have even been known to attend a Saturday morning Katathon).", "When I'm not debugging, refactoring or otherwise getting to grips with the latest technologies, I love to get outdoors. I'm a keen swimmer and a slightly less than enthusiastic runner, though I did struggle through the London Marathon this year—I'm nothing if not persevering."]
     }
   ]
 }
