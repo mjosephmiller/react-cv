b7f601768d85382054c3c5bb45f4b303103273b0
Michael Miller <michael.miller@bilendo.de>
2017-08-28 22:37:00 +0200

    adds hotloading

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     loaders: [{
       exclude: /node_modules/,
-      loader: 'babel',
+      loader: 'babel-loader',
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
@@ -21,6 +21,8 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: './'
+    contentBase: './',
+    open: true,
+    port: 3030
   }
 };
